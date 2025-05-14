5deeba8ba4a1f0f431705f5df4f7b5859dc78946
Abhinandan-pixel <abhinandanmysore@gmail.com>
2025-05-14 17:04:29 +0530

    Angular skeleton complete

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //adds sqlite database to the service container
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); //allows any header and method from the specified origin
 
 app.MapControllers(); //maps the controllers to the app
 
