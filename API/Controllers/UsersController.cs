6fb83e1727593d343b6244d7756bb86ad330e9e7
Abhinandan-pixel <abhinandanmysore@gmail.com>
2025-06-05 20:00:51 +0530

    Improved UI , Added Edit page and added PUT API for updating user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -10,8 +11,8 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController: BaseApiController
-    {   
+    public class UsersController : BaseApiController
+    {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
 
@@ -25,7 +26,7 @@ namespace API.Controllers
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetUsersAsync();
-            //_mapper.Map<What to map to>(what to map from)
+            //_mapper.Map<What to map to>(what to map from) 
             var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
             return Ok(usersToReturn);
         }
@@ -34,6 +35,18 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            if (user == null) return NotFound();
+            _mapper.Map(memberUpdateDto, user); // overwrites data in user with data from memberUpdateDto
+            if (await _userRepository.SaveAllAsync())
+            {
+                return NoContent(); // 204 No Content
+            }
+            return BadRequest("Failed to update user");
         }
     }
 }
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,6 +4,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [],
@@ -18,13 +19,15 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
       progressBar: true,
       timeOut: 3000
     }),
-    NgxGalleryModule
+    NgxGalleryModule,
+    NgxSpinnerModule.forRoot({ type: 'line-scale-party' })
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
     TabsModule,
-    NgxGalleryModule
+    NgxGalleryModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
